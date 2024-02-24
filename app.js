c7366f0583c746da1ceb0a80f34d4dfa32b22866
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2024-02-24 22:23:37 +0530

    changes port for vercel

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const Jimp = require('jimp')
 const app = express(); 
-const port = 3001; 
+const port = process.env.PORT || 3000;
 
 
 
